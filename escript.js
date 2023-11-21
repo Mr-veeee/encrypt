f911fc68a78f3908c6cf4b4580d80659e9f756cf
Mr-veeee <128126140+Mr-veeee@users.noreply.github.com>
2023-11-21 11:49:24 +0530

    Add files via upload

---
+++ escript.js
@@ -0,0 +1,22 @@
+function encrypt() {
+    const message = document.getElementById('message').value;
+    const key = document.getElementById('key').value;
+
+    const encryptedMessage = CryptoJS.AES.encrypt(message, key).toString();
+    document.getElementById('encrypted').value = encryptedMessage;
+}
+
+function decrypt() {
+    const encryptedMessage = document.getElementById('encrypted').value;
+    const key = document.getElementById('key').value;
+
+    const decryptedMessage = CryptoJS.AES.decrypt(encryptedMessage, key).toString(CryptoJS.enc.Utf8);
+    document.getElementById('decrypted').value = decryptedMessage;
+}
+
+function shareWhatsApp() {
+    const encryptedMessage = document.getElementById('encrypted').value;
+    const whatsappLink = `whatsapp://send?text=${encryptedMessage}`;
+
+    window.open(whatsappLink, '_blank');
+}
